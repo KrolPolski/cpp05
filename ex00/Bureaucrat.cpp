2a5f3e5ec58db37cc29b2b31c4ed3ec63e3210f0
Ryan Boudwin <rboudwin@student.hive.fi>
2024-11-07 20:05:06 +0200

    Added exception classes

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,29 +6,59 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:03:52 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/11/05 17:07:50 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:16:10 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Mindless Drone"), _grade(150)
+{
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade Too High: 1 is the maximum grade for a bureaucrat";
+}
+
 Bureaucrat::Bureaucrat(int grade)
 {
 	try
 	{
-		
+		if (grade < 1)
+			throw GradeTooHighException();
+		else if (grade > 150)
+			throw GradeTooLowException();	
 	}
 	
-	catch(const std::exception& e)
+	catch(const GradeTooHighException& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 	
 }
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name) 
+{
+	_grade = other._grade;
+}
 
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	
+}
+	
 Bureaucrat::~Bureaucrat()
 {
 }
-
+void	Bureaucrat::IncrementGrade()
+{
+	
+}
+void	Bureaucrat::DecrementGrade()
+{
+	
+}
+	
 int Bureaucrat::getGrade() const
 {
 	return grade;
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,18 +6,40 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:01:32 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/11/05 17:06:58 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:15:02 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+
 class Bureaucrat
 {
 private:
-	const std::string	name;
-	int			 		grade;
+	const std::string	_name;
+	int			 		_grade;
 public:
+	class GradeTooHighException : std::exception
+	{
+		public:
+			GradeTooHighException() = default;
+			virtual const char *what() const throw();
+			~GradeTooHighException() = default;
+	};
+	
+	class GradeTooLowException : std::exception
+	{
+		public:
+			GradeTooLowException() = default;
+			virtual const char *what() const throw();
+			~GradeTooLowException() = default;
+	};
+
+	Bureaucrat();
 	Bureaucrat(int grade);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	void IncrementGrade();
+	void DecrementGrade();
 	~Bureaucrat();
 	int getGrade() const;
 	const std::string getName() const;
